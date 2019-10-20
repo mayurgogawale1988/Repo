906fd50c8515f48f83b5fd5ca05a0b5fb0172b63
root <root@ip-172-31-44-253.us-east-2.compute.internal>
2019-10-20 05:40:12 +0000

    ss

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Created New Server..Host your sites \n");
+  response.end("Hello Created New Server..Host your sites ........................Add own code and start running app on mobile\n");
 });
 
 // listen on localhost:8000
